13e8bbd4787bc883afb59f29ee021b86ac17b0a4
Vincent Faury <vincent.faury@gmail.com>
2019-03-20 09:14:37 +0100

    Init

--- app/src/main/java/fr/vfaury/retrofitpokemon/MainActivity.java
+++ app/src/main/java/fr/vfaury/retrofitpokemon/MainActivity.java
@@ -1,13 +1,63 @@
 package fr.vfaury.retrofitpokemon;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-import fr.vfaury.retrofitpokemon.Controller;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity extends AppCompatActivity {
-        public void main(String[] args) {
-            Controller controller = new Controller();
-            controller.start();
-        }
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        Gson gson = new GsonBuilder().setLenient().create();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://pokeapi.co/api/v2/").addConverterFactory(GsonConverterFactory.create(gson)).build();
+        GerritAPI pokemonRestApi = retrofit.create(GerritAPI.class);
+        Call<RestPokemonResponse> call = pokemonRestApi.getListPokemon();
+
+        call.enqueue(new Callback<RestPokemonResponse>() {
+            @Override
+            public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
+                RestPokemonResponse restPokemonResponse = response.body();
+                List<Pokemon> listPokemon = restPokemonResponse.getResults();
+                showList(listPokemon);
+            }
+
+            @Override
+            public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+                Log.d("Erreur", "API KO");
+            }
+        });
+    }
+
+    private void showList(List<Pokemon> list){
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        mAdapter = new MyAdapter(list);
+        recyclerView.setAdapter(mAdapter);
+    }
 }
---
+++ app/src/main/java/fr/vfaury/retrofitpokemon/MyAdapter.java
@@ -0,0 +1,72 @@
+package fr.vfaury.retrofitpokemon;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<Pokemon> values;
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        // each data item is just a string in this case
+        public TextView txtHeader;
+        public TextView txtFooter;
+        public ImageView img;
+        public View layout;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout = v;
+            txtHeader = (TextView) v.findViewById(R.id.firstLine);
+            txtFooter = (TextView) v.findViewById(R.id.secondLine);
+            img = v.findViewById(R.id.icon);
+        }
+    }
+
+    public void add (int position, Pokemon item) {
+        values.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove (int position) {
+        values.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public MyAdapter(List<Pokemon> values){
+        this.values = values;
+    }
+
+    @NonNull
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewtype) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v =inflater.inflate(R.layout.row_layout, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+        final Pokemon pokemon = values.get(position);
+        holder.txtHeader.setText(pokemon.getName());
+        holder.txtHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(position);
+            }
+        });
+        holder.txtFooter.setText("Footer: "+ pokemon);
+    }
+
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+}
